8b4b6f9952b85c7e2146e7e96bf6369751796ff7
sumit kumar <sumokhan76342@gmail.com>
2023-03-25 13:03:05 +0530

    navbar

--- swiggy/src/components/Navbar.js
+++ swiggy/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react' //why we are importing React //* impr
 import PropTypes from 'prop-types' //* impt
+import "./nav.css"
 
 
 export default function Navbar(props) {
@@ -18,12 +19,16 @@ export default function Navbar(props) {
                         <li className="nav-item">
                             <a className="nav-link disabled" href="/">{props.Location}</a>
                         </li>
-                        <form className="d-flex search" >
-                            <i class="bi bi-search"></i>
+                        <form className="d-flex search " >
+                            {/* <i class="bi bi-search"></i> */}
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                             </svg>
-                            <button className="btn btn-outline-success" type="submit">Search</button>
+                            <span class="search_name">Search</span>
+                            <span className="offer">offer</span>
+                            <span className="Help">Help</span>
+                            <span className="Sign">Sign in</span>
+                            <span className="cart">cart</span>
                         </form>
                     </ul>
 
